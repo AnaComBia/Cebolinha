86297ccc0c002236966039a60cc0e0f81fda491c
AnaComBia <anabeatrizetorres@gmail.com>
2021-08-31 23:20:18 -0300

    finalizar programa cebolinha

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Escreva algo e eu vou demonstrar como o cebolinha falaria: ");
+        
+            String usuariofalar = Console.ReadLine();
+            String usuariofalal = usuariofalar.Replace("r", "l");
+
+            Console.WriteLine(usuariofalal);
+        }
+    }
+}
