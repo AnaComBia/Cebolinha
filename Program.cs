8e56a6bba54b3646064979e40dc6bdc77211ea25
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-01 12:27:31 -0300

    finaliza programa Cebolinha

--- Program.cs
+++ Program.cs
@@ -7,17 +7,15 @@ namespace Cebolinha
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Escreva algo e eu vou demonstrar como o cebolinha falaria: ");
+            Console.WriteLine("\nEscreva algo e eu vou demonstrar como o cebolinha falaria: ");
         
             String usuariofalar = Console.ReadLine();
             String usuariofalal = usuariofalar
             .Replace("r", "l")
             .Replace("R", "L");
 
-
-            Console.Write("\njeito do cebolinha: ");
-            Console.Write(usuariofalal);
-            Console.WriteLine();
+            Console.Write($"\njeito do cebolinha: {usuariofalal}\n");
+            
         }
     }
 }
